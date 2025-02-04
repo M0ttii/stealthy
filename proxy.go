3995810ddd9f8e2ffeb6fabae3b3df19ecbdeb86
Adrian <adrian.doernberg@berlin.de>
2025-02-04 12:49:54 +0100

    Added proxy rotation

--- proxy.go
+++ proxy.go
@@ -3,6 +3,8 @@ package stealthy
 import (
 	"fmt"
 	"net/url"
+	"strconv"
+	"strings"
 )
 
 type ProxyConfig struct {
@@ -38,4 +40,53 @@ func WithProxy(cfg ProxyConfig) func(*StealthClient) {
 			cfg.Port)
 
 	}
+}
+
+func (c *StealthClient) RotateProxySession() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	newSessionID := generateSessionID(5)
+
+	if strings.HasPrefix(c.serializable.Proxy, "http://") {
+
+		proxyURL, err := url.Parse(c.serializable.Proxy)
+		if err != nil {
+			return err
+		}
+
+		userInfo := strings.Split(proxyURL.User.String(), ":")
+		if len(userInfo) != 2 {
+			return fmt.Errorf("invalid user info format")
+		}
+
+		usernameParts := strings.Split(userInfo[0], "-")
+		if len(usernameParts) < 5 {
+			return fmt.Errorf("invalid smartproxy username format")
+		}
+
+		sessionDuration, err := strconv.Atoi(usernameParts[5])
+
+		port, err := strconv.Atoi(proxyURL.Port())
+		if err != nil {
+			return fmt.Errorf("invalid proxy port: %v", err)
+		}
+
+		cfg := ProxyConfig{
+			Host:            proxyURL.Hostname(),
+			User:            strings.Join(usernameParts[1:2], "-"), // "sp463pynue"
+			ZonePassword:    userInfo[1],
+			SessionDuration: sessionDuration,
+			Port:            port,
+		}
+
+		// Neue Session ID setzen
+		c.sessionID = newSessionID
+		c.serializable.SessionID = newSessionID
+
+		// Proxy neu konfigurieren
+		WithProxy(cfg)(c)
+	}
+
+	return nil
 }
